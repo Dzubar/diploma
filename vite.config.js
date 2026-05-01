02b9a475be9d0dfeac034aedffd7c4afc0d58b3c
Dzubar <d.zubar@gmail.com>
2026-05-01 14:20:36 +0300

    Updated index.html

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  }
+})
